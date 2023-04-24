f9ee881a88294c63232c87f2f29d812ae0386c89
bluew <bluewww@users.noreply.github.com>
2023-04-24 02:53:48 +0200

    Add iis simulation environment script

---
+++ scripts/env-iis.sh
@@ -0,0 +1,9 @@
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+#
+
+# set up environment variables for rtl simulation
+ROOTD=$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")/.." && pwd)
+export RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0
+export QUESTA=questa-2022.3
