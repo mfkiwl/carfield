0c5d09bf142fe9eeaa054ef34f9d3fb95ce991dd
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2024-09-04 16:13:12 +0200

    Bump `tech` commit

--- carfield.mk
+++ carfield.mk
@@ -422,7 +422,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@iis-git.ee.ethz.ch:Astral/gf12.git
 # no commit by default, change during development
-tech-commit := 5473df37e531b3c5111024bd471abb97e356a9e0
+tech-commit := 183a9cde1130f2558b359619377c0afa06a7c571 # branch: yt/thales
 
 tech-clone:
 	git clone $(tech-repo) tech
