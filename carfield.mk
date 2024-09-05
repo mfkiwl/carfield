3b183d7b9c2074673f9cbff5ede4615f638289f8
Yvan Tortorella <ytortorella@larain9.ee.ethz.ch>
2024-09-05 23:28:58 +0200

    Bump tech.

--- carfield.mk
+++ carfield.mk
@@ -422,7 +422,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@iis-git.ee.ethz.ch:Astral/gf12.git
 # no commit by default, change during development
-tech-commit := a19cf025f2c2d4ca6e529adca2ea3bca6897933b # branch: yt/thales
+tech-commit := 94cf830f98c683874093db26cd587564035b85c4 # branch: yt/thales
 
 tech-clone:
 	git clone $(tech-repo) tech
