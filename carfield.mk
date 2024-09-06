5fcf1a494e2332afb3e1829ab27ee613985535da
Yvan Tortorella <ytortorella@larain9.ee.ethz.ch>
2024-09-06 15:37:28 +0200

    Update tech to remove double/triple height cells from PDK.

--- carfield.mk
+++ carfield.mk
@@ -422,7 +422,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@iis-git.ee.ethz.ch:Astral/gf12.git
 # no commit by default, change during development
-tech-commit := 94cf830f98c683874093db26cd587564035b85c4 # branch: yt/thales
+tech-commit := b3a8d136dd53122507083b653e6ea32a2e16c8a2 # branch: yt/thales
 
 tech-clone:
 	git clone $(tech-repo) tech
